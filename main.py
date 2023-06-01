1bdbf0d82dc0f1d83e81d86b537cc938e2135f0d
nidhisin333 <133700565+nidhisin333@users.noreply.github.com>
2023-06-01 21:37:11 +0000

    items call

--- main.py
+++ main.py
@@ -16,3 +16,21 @@ data = response.json()
 trace("\nHere are all the kay/value pairs in the JSON response:")
 for key, value in data.items():
   trace (key, ": ", value)
+
+
+# -- ITEMS -- #
+URL = "https://pokeapi.co/api/v2/item/"
+
+def trace(*args):
+  #for debug output
+  print(*args) #comment this line out later to remove debug output
+  pass
+
+trace("Calling", URL)
+response = requests.get(URL)
+response.raise_for_status()
+data = response.json()
+
+trace("\nHere are all the kay/value pairs in the JSON response:")
+for key, value in data.items():
+  trace (key, ": ", value)
