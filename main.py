f04ce0daf8ff613a3aabfb9792f37c86a31c11fb
alliech <133700697+alliech@users.noreply.github.com>
2023-06-01 22:01:13 +0000

    pikachu pokemon desc

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import requests
 
+# -- DESCRIPTION/POKEDEX -- #
+URL = "https://pokeapi.co/api/v2/pokemon/pikachu"
+
+def trace(*args):
+  #for debug output
+  print(*args)
+  pass
+
+trace("Calling", URL)
+response = requests.get(URL)
+response.raise_for_status()
+data = response.json()
+
+trace("\nHere are all the key/value pairs in the JSON response:")
+for key, value in data.items():
+  trace (key, ": ", value)
+
 # -- ABILITIES -- #
 URL = "https://pokeapi.co/api/v2/ability/static"
 
