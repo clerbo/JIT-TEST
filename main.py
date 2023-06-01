181c5bcc0585ea1b8fdad2164fa73876e5167b45
alliech <133700697+alliech@users.noreply.github.com>
2023-06-01 21:27:47 +0000

    alla

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ response = requests.get(URL)
 response.raise_for_status()
 data = response.json()
 
+<<<<<<< HEAD
 trace("\nHere are all the kay/value pairs in the JSON response:")
 for key, value in data.items():
   trace (key, ": ", value)
+=======
+>>>>>>> 635f098f8854eec510b923a72dcb5616a448d40c
