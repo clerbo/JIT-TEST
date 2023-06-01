e41d4d6d88f2d563389876167d1e4fcb6de4f451
alliech <133700697+alliech@users.noreply.github.com>
2023-06-01 21:24:57 +0000

    trace statements

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import requests
 # -- ABILITIES -- #
 URL = "https://pokeapi.co/api/v2/ability/"
 
+def trace(*args):
+  #for debug output
+  print(*args) #comment this line out later to remove debug output
+  pass
+
+trace("Calling", URL)
 response = requests.get(URL)
 response.raise_for_students()
 data = response.json()
+
+trace("\nHere are all the kay/value pairs in the JSON response:")
+for key, value in data.items():
+  trace (key, ": ", value)
