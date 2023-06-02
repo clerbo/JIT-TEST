5e6c54981ad45057667657d78daba2e31c6e4b73
alliech <133700697+alliech@users.noreply.github.com>
2023-06-02 16:35:54 +0000

    lala

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import requests
-"""
+
+import pypokedex
+import urllib3
+import tkinter as tk
+import PIL.image
+from io import BytesIO 
+
 # -- DESCRIPTION/POKEDEX -- #
-URL = "https://pokeapi.co/api/v2/pokemon/pikachu"
+
+name = input("What Pokemon would you like to know more about?")
+pokemon = pypokedex.get(name) 
+print(pokemon.dex)
+print(pokemon.name)
+print(pokemon.types)
 
 def trace(*args):
-  #for debug output
+  for debug output
   print(*args)
   pass
 
@@ -16,12 +27,12 @@ data = response.json()
 trace("\nHere are all the key/value pairs in the JSON response:")
 for key, value in data.items():
   trace (key, ": ", value)
-"""
+
 # -- ABILITIES -- #
 URL = "https://pokeapi.co/api/v2/ability/static"
 
 def trace(*args):
-  #for debug output
+  for debug output
   print(*args)
   pass
 
@@ -30,31 +41,22 @@ response = requests.get(URL)
 response.raise_for_status()
 data = response.json()
 
-print (data["effect_changes"])
-effect_changes = data["effect_changes"]
-for entry in effect_changes:
-  print ()
-  print (entry["effect_entries"])
-  for item in entry["effect_entries"]:
-    if item["language"]["name"] == "en":
-      print (item["effect"])
-
-#def name(params):
-  #URL = f'https://pokeapi.co/api/v2/ability?q={name}'
-  #response = requests.get(URL)
-  #
+def name(params):
+  URL = f'https://pokeapi.co/api/v2/ability?q={name}'
+  response = requests.get(URL)
+  
 
 trace("\nHere are all the key/value pairs in the JSON response:")
-#for key, value in data.items():
-#  trace (key, ": ", value)
-  
-"""
+for key, value in data.items():
+  trace (key, ": ", value)
+
+
 # -- ITEMS -- #
 URL = "https://pokeapi.co/api/v2/item/master-ball/"
 
 def trace(*args):
-  #for debug output
-  #print(*args) #comment this line out later to remove debug output
+  for debug output
+  print(*args) #comment this line out later to remove debug output
   pass
 
 trace("Calling", URL)
@@ -65,4 +67,3 @@ data = response.json()
 trace("\nHere are all the kay/value pairs in the JSON response:")
 for key, value in data.items():
   trace (key, ": ", value)
-"""
