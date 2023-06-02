293037c9098fc48f0bd061e373a419b88ac722d2
alliech <133700697+alliech@users.noreply.github.com>
2023-06-02 16:31:02 +0000

    effect description oNLY

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+"""
 # -- DESCRIPTION/POKEDEX -- #
 URL = "https://pokeapi.co/api/v2/pokemon/pikachu"
 
@@ -16,7 +16,7 @@ data = response.json()
 trace("\nHere are all the key/value pairs in the JSON response:")
 for key, value in data.items():
   trace (key, ": ", value)
-
+"""
 # -- ABILITIES -- #
 URL = "https://pokeapi.co/api/v2/ability/static"
 
@@ -30,11 +30,25 @@ response = requests.get(URL)
 response.raise_for_status()
 data = response.json()
 
-trace("\nHere are all the key/value pairs in the JSON response:")
-for key, value in data.items():
-  trace (key, ": ", value)
-  
+print (data["effect_changes"])
+effect_changes = data["effect_changes"]
+for entry in effect_changes:
+  print ()
+  print (entry["effect_entries"])
+  for item in entry["effect_entries"]:
+    if item["language"]["name"] == "en":
+      print (item["effect"])
 
+#def name(params):
+  #URL = f'https://pokeapi.co/api/v2/ability?q={name}'
+  #response = requests.get(URL)
+  #
+
+trace("\nHere are all the key/value pairs in the JSON response:")
+#for key, value in data.items():
+#  trace (key, ": ", value)
+  
+"""
 # -- ITEMS -- #
 URL = "https://pokeapi.co/api/v2/item/master-ball/"
 
@@ -51,3 +65,4 @@ data = response.json()
 trace("\nHere are all the kay/value pairs in the JSON response:")
 for key, value in data.items():
   trace (key, ": ", value)
+"""
