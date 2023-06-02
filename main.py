756fd34ca7065086597790e1e6678db828bbb07c
alliech <133700697+alliech@users.noreply.github.com>
2023-06-02 16:53:48 +0000

    lala

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import requests
-
 import pypokedex
 import urllib3
 import tkinter as tk
-import PIL.image
 from io import BytesIO 
 
 # -- DESCRIPTION/POKEDEX -- #
 
-name = input("What Pokemon would you like to know more about?")
-pokemon = pypokedex.get(name) 
-print(pokemon.dex)
-print(pokemon.name)
-print(pokemon.types)
+n = input("What Pokemon would you like to know more about?")
+pokemon = pypokedex.get(name = str(n)) 
+print("The Pokedex number for this Pokemon is " + str(pokemon.dex) + ".")
+print("These are the types that this Pokemon is: " + str(pokemon.types))
+print("This Pokemon weighs " + str(pokemon.weight) + " pounds.")
+
 
 def trace(*args):
-  for debug output
-  print(*args)
-  pass
+  for debug in output:
+    print(*args)
+    pass
 
 trace("Calling", URL)
 response = requests.get(URL)
@@ -29,35 +28,30 @@ for key, value in data.items():
   trace (key, ": ", value)
 
 # -- ABILITIES -- #
-URL = "https://pokeapi.co/api/v2/ability/static"
+URL = https://pokeapi.co/api/v2/ability/static
 
 def trace(*args):
-  for debug output
-  print(*args)
-  pass
+  for debug in output:
+    print(*args)
+    pass
 
 trace("Calling", URL)
 response = requests.get(URL)
 response.raise_for_status()
 data = response.json()
 
-def name(params):
-  URL = f'https://pokeapi.co/api/v2/ability?q={name}'
-  response = requests.get(URL)
-  
-
 trace("\nHere are all the key/value pairs in the JSON response:")
 for key, value in data.items():
   trace (key, ": ", value)
-
+  
 
 # -- ITEMS -- #
-URL = "https://pokeapi.co/api/v2/item/master-ball/"
+URL = https://pokeapi.co/api/v2/item/master-ball/
 
 def trace(*args):
-  for debug output
-  print(*args) #comment this line out later to remove debug output
-  pass
+  for debug in output:
+    print(*args) #comment this line out later to remove debug output
+    pass
 
 trace("Calling", URL)
 response = requests.get(URL)
